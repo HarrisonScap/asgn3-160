2b7019ec8823c5ca29e8216b60c8676d6978dae0
Harrison S <hscapell@ucsc.edu>
2024-04-29 01:43:46 -0700

    Updated

--- asg3.js
+++ asg3.js
@@ -207,6 +207,68 @@ class Cube{
 
         drawTriangle3D([1.0,1.0,1.0, 1.0,0.0,1.0, 1.0,1.0,0.0]);
         drawTriangle3D([1.0,0.0,0.0, 1.0,0.0,1.0, 1.0,1.0,0.0]);
+
+        gl.uniform4f(u_FragColor,rgba[0]*.9,rgba[1]*.9,rgba[2]*.9,rgba[3]);
+
+        drawTriangle3D([0.0,1.0,0.0, 1.0,1.0,0.0, 0.0,0.0,0.0]);
+        drawTriangle3D([1.0,0.0,0.0, 1.0,1.0,0.0, 0.0,0.0,0.0]);
+
+        gl.uniform4f(u_FragColor,rgba[0]*.9,rgba[1]*.9,rgba[2]*.9,rgba[3]);
+
+        drawTriangle3D([0.0,1.0,1.0, 0.0,0.0,1.0, 1.0,1.0,1.0]);
+        drawTriangle3D([1.0,0.0,1.0, 0.0,0.0,1.0, 1.0,1.0,1.0]);
+
+        gl.uniform4f(u_FragColor,rgba[0]*.9,rgba[1]*.9,rgba[2]*.9,rgba[3]);
+
+        drawTriangle3D([1.0,0.0,1.0, 0.0,0.0,0.0, 0.0,0.0,1.0]);
+        drawTriangle3D([1.0,0.0,1.0, 0.0,0.0,0.0, 1.0,0.0,0.0]);
+
+        gl.uniform4f(u_FragColor,rgba[0]*.9,rgba[1]*.9,rgba[2]*.9,rgba[3]);
+
+        drawTriangle3D([0.0,0.0,0.0, 0.0,0.0,1.0, 0.0,1.0,0.0]);
+        drawTriangle3D([0.0,1.0,1.0, 0.0,1.0,0.0, 0.0,0.0,1.0]);
+
+    }
+}
+
+// My second "primitive shape"
+class Pyramid{
+    constructor(){
+        this.type = "pyramid";
+        //this.position = [0.0,0.0,0.0];
+        this.color = [1.0,1.0,1.0,1.0];
+        //this.size = 5.0;
+        //this.segments = g_segmentCount;
+        this.matrix = new Matrix4();
+    }
+
+    render(){
+        //var xy = this.position;
+        var rgba = this.color;
+        //var size = this.size;
+
+        // Pass the color of a point to u_FragColor variable
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+        gl.uniformMatrix4fv(u_ModelMatrix,false,this.matrix.elements);
+        
+        drawTriangle3D([0.0,0.0,0.0, 1.0,0.0,0.0, 0.0,0.0,1.0]);
+        drawTriangle3D([1.0,0.0,1.0, 1.0,0.0,0.0, 0.0,0.0,1.0]);
+        
+        gl.uniform4f(u_FragColor,rgba[0]*.9,rgba[1]*.9,rgba[2]*.9,rgba[3]);
+        drawTriangle3D([0.0,0.0,0.0, 1.0,0.0,0.0, 0.5,1,.5]);
+
+        gl.uniform4f(u_FragColor,rgba[0]*.8,rgba[1]*.8,rgba[2]*.8,rgba[3]);
+        drawTriangle3D([1.0,0.0,0.0, 1.0,0.0,1.0, 0.5,1,.5]);
+        
+        gl.uniform4f(u_FragColor,rgba[0]*.7,rgba[1]*.7,rgba[2]*.7,rgba[3]);
+        drawTriangle3D([0.0,0.0,0.0, 0.0,0.0,1.0, 0.5,1,.5]);
+
+        gl.uniform4f(u_FragColor,rgba[0]*.6,rgba[1]*.6,rgba[2]*.6,rgba[3]);
+        drawTriangle3D([0.0,0.0,1.0, 1.0,0.0,1.0, 0.5,1,.5]);
+
+
+
     }
 }
 
@@ -298,32 +360,122 @@ function renderAllShapes(){
     // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.clear(gl.COLOR_BUFFER_BIT);
-
+    
     var body = new Cube();
     body.color = [1.0,0.0,0.0,1.0];
-    body.matrix.translate(-.25,-.75,0.0);
-    body.matrix.rotate(-5,1,0,0);
-    body.matrix.scale(.5,.3,.5);
-    body.render();
+    body.matrix.translate(-.25,-.3,0.0);
+    body.matrix.rotate(0,1,0,0)
+    body.matrix.rotate(-g_bodyAngle,1,0,0)
+    var bodyCoords = new Matrix4(body.matrix);
+    body.matrix.scale(.5,.6,.3);
+    body.render(); 
 
-    var leftArm = new Cube();
-    leftArm.color = [1.0,1.0,0.0,1.0];
-    leftArm.matrix.translate(0,-.5,0.0);
-    leftArm.matrix.rotate(-5,1,0,0);
-    leftArm.matrix.rotate(-g_yellowAngle,0,0,1);
-    var armCoords = new Matrix4(leftArm.matrix);
-    leftArm.matrix.scale(.25,.7,.5);
-    leftArm.matrix.translate(-.5,0,0)
-    leftArm.render();
+    var arm2 = new Cube();
+    arm2.color = [0.5,0.0,0.0,1.0];
+    arm2.matrix = new Matrix4(bodyCoords);
+    arm2.matrix.translate(.6,0,.05)
+    arm2.matrix.rotate(180,0,0,1)
+    arm2.matrix.rotate(180,0,1,0)
+    arm2.matrix.translate(-.1,-.6,-.2)
+    arm2.matrix.rotate(-g_armAngle,0,0,1)
+    var arm2Coords = new Matrix4(arm2.matrix)
+    arm2.matrix.scale(.2,.6,.2);
+    arm2.render();
 
-    var box = new Cube();
-    box.color = [1,0,1,1];
-    box.matrix = armCoords;
-    box.matrix.translate(0,.65,0);
-    box.matrix.rotate(-g_magentaAngle,0,0,1);
-    box.matrix.scale(.3,.3,.3);
-    box.matrix.translate(-.5,0,-.0001);
-    box.render();
+    var hand1 = new Cube();
+    hand1.color = [1.0,1.0,0.0,1.0];
+    hand1.matrix = new Matrix4(arm2Coords);
+    hand1.matrix.scale(.15,.15,.05)
+    hand1.matrix.translate(0,4,1.5);
+    hand1.matrix.rotate(-g_handAngle,0,0,1)
+    hand1.render();
+
+    var hand2 = new Cube();
+    hand2.color = [1.0,1.0,0.0,1.0];
+    hand2.matrix = new Matrix4(bodyCoords);
+    hand2.matrix.scale(.15,.15,.05)
+    hand2.matrix.translate(-1,-1,2);
+    hand2.render();
+
+    var arm3 = new Cube();
+    arm2.color = [0.5,0.0,0.0,1.0];
+    arm2.matrix = new Matrix4(bodyCoords);
+    arm2.matrix.translate(-.2,0,.05)
+    arm2.matrix.scale(.2,.6,.2);
+    arm2.render();
+
+    var leg1 = new Cube();
+    leg1.color = [0.0,0.0,.5,1.0];
+    leg1.matrix.translate(.05,-.9,.05)
+    leg1.matrix.scale(.2,.6,.2);
+    leg1.render();
+
+    var leg2 = new Cube();
+    leg2.color = [0.0,0.0,.5,1.0];
+    leg2.matrix.translate(-.25,-.9,.05)
+    leg2.matrix.scale(.2,.6,.2);
+    leg2.render();
+
+
+
+    // Head //
+
+    var head = new Cube();
+    head.color = [1.0,1.0,0.0,1.0];
+    head.matrix = bodyCoords;
+    head.matrix.translate(.1,.6,0);
+    head.matrix.rotate(-g_headAngle,1,0,0);
+    var headCoords = new Matrix4(head.matrix);
+    head.matrix.scale(.3,.3,.3);
+    head.render(); 
+
+    var hat = new Pyramid();
+    hat.color = [1,0,0,1];
+    hat.matrix = new Matrix4(headCoords);
+    hat.matrix.translate(0,.3,0)
+    hat.matrix.scale(.3,.3,.3);
+    hat.matrix.translate(0,g_hatTranslate,0)
+    hat.render();
+
+    var eye1 = new Cube();
+    eye1.color = [.3,.3,.3,1];
+    eye1.matrix = new Matrix4(headCoords);
+    eye1.matrix.translate(0,.15,-.05)
+    eye1.matrix.scale(.12,.1,.05);
+    eye1.render();
+
+    var eye2 = new Cube();
+    eye2.color = [.3,.3,.3,1];
+    eye2.matrix = new Matrix4(headCoords);
+    eye2.matrix.translate(.18,.15,-.05)
+    eye2.matrix.scale(.12,.1,.05);
+    eye2.render();
+
+    var eye3 = new Cube();
+    eye3.color = [.3,.3,.3,1];
+    eye3.matrix = new Matrix4(headCoords);
+    eye3.matrix.translate(0.1,.2,-.05)
+    eye3.matrix.scale(.12,.05,.05);
+    eye3.render();
+
+    /*
+    var eye2 = new Cube();
+    eye2.color = [.3,.3,.3,1];
+    eye2.matrix = new Matrix4(headCoords);
+    eye2.matrix.translate(1.4,0,0)
+    eye2.matrix.scale(1.1,1,1);
+    eye2.render();
+
+    var eye3 = new Cube();
+    eye3.color = [.3,.3,.3,1];
+    eye3.matrix = new Matrix4(headCoords);
+    eye3.matrix.translate(-.45,.6,0)
+    eye3.matrix.scale(.6,.4,.4);
+    eye3.render();
+    */
+
+    //////////////////////////////////////////////////////////
+
 
     var duration = performance.now() - startTime;
     sendTextToHTML("ms: " + Math.floor(duration) + " fps: " + Math.floor(10000/duration), "numdot");
@@ -346,59 +498,81 @@ const CIRCLE = 2;
 
 // Global Variables
 let g_globalAngle = 0;
-let g_yellowAngle = 0;
-let g_magentaAngle = 0;
+let g_bodyAngle = 0;
+let g_headAngle = 0;
+let g_armAngle = 0;
+let g_handAngle = 0;
 let animation = false;
+let g_hatTranslate = 0
 
 function addActionsForHtmlUI(){
 
     document.getElementById("angleSlide").addEventListener('mousemove',function() {g_globalAngle = this.value; renderAllShapes(); });
-    document.getElementById("yellowSlide").addEventListener('mousemove',function() {g_yellowAngle = this.value; renderAllShapes(); });
-    document.getElementById("magentaSlide").addEventListener('mousemove',function() {g_magentaAngle = this.value; renderAllShapes(); });
+    document.getElementById("body").addEventListener('mousemove',function() {g_bodyAngle = this.value; renderAllShapes(); });
+    document.getElementById("arm").addEventListener('mousemove',function() {g_armAngle = this.value; renderAllShapes(); });
+    document.getElementById("hand").addEventListener('mousemove',function() {g_handAngle = this.value; renderAllShapes(); });
+    document.getElementById("head").addEventListener('mousemove',function() {g_headAngle = this.value; renderAllShapes(); });
     
 
     document.getElementById("animation").onclick = function() {animation = !animation;};
 
 }
 
-
+function renderScene(){
+    setupWebGL();
+    connectVariablesToGLSL();
+    addActionsForHtmlUI();
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    requestAnimationFrame(tick);
+}
 
 function main() {
-
-  // Sets up canvas and gl vars
-  setupWebGL();
-  // Set up GLSL shader programs
-  connectVariablesToGLSL();
-
-  addActionsForHtmlUI();
-
-  // Register function (event handler) to be called on a mouse press
-  //canvas.onmousedown = click;
-  canvas.onmousemove = function(ev) { if(ev.buttons == 1) {click(ev) }}
-
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // Clear <canvas>
-  //gl.clear(gl.COLOR_BUFFER_BIT);
-  //renderAllShapes();
-  requestAnimationFrame(tick);
+  renderScene();
 }
 
 var g_startTime = performance.now()/1000.0
 var g_seconds=performance.now()/1000.0-g_startTime;
+var timer = 0;
 
 function tick() {
 
     g_seconds=performance.now()/1000-g_startTime;
-    console.log(performance.now());
+
+    canvas.onmousedown = function(ev){
+        console.log("HERE")
+        if(ev.buttons){
+            console.log(ev.clientX);
+            g_globalAngle.x = ev.clientX;
+            g_globalAngle.y = ev.clientY;
+        }
+    }
+
+
     updateAnimationAngles();
+    canvas.onmousedown = function(ev){
+        if((ev.shiftKey && ev.buttons)){
+            g_hatTranslate = Math.abs(Math.sin(g_seconds - timer))
+            timer = g_seconds;
+            console.log(timer)
+        }
+    }
+
+    if(g_hatTranslate != 0 && (g_seconds - timer < 3)){
+        g_hatTranslate = Math.abs(Math.sin(g_seconds - timer))
+    }else{
+        g_hatTranslate = 0;
+    }
+
+
     renderAllShapes();
     requestAnimationFrame(tick);
 }
 
 function updateAnimationAngles(){
     if(animation){
-        g_yellowAngle = 45*Math.sin(g_seconds);
+        g_bodyAngle = 45*Math.sin(g_seconds*10);
+        g_armAngle = Math.abs(-30*Math.sin(g_seconds));
+        g_headAngle = Math.abs(45*Math.sin(g_seconds*5));
     }
 }
+
